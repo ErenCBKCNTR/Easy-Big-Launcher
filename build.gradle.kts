82a0640da574c3e12adc40810cbc4ec6170108d8
ErenCBKCNTR <erencs87@gmail.com>
2026-05-17 20:59:43 +0300

    build: Update Android Gradle plugin versions
    Upgrades the Android Gradle plugin and Kotlin versions in the root build.gradle.kts file. This ensures compatibility with newer Android development features and tooling.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Root build.gradle.kts
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
