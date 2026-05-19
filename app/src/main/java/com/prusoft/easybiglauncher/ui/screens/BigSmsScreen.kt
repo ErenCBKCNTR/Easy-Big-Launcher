81abc463a58b474be889bff2f412c0fa6708f0a3
ErenCBKCNTR <erencs87@gmail.com>
2026-05-20 00:37:54 +0300

    feat: Add large font size for SMS reply button
    Increases the font size of the "Reply Send" button text to 32sp for better visibility and accessibility.

--- app/src/main/java/com/prusoft/easybiglauncher/ui/screens/BigSmsScreen.kt
+++ app/src/main/java/com/prusoft/easybiglauncher/ui/screens/BigSmsScreen.kt
@@ -395,7 +395,6 @@ fun BigSmsScreen(navController: NavController, viewModel: com.prusoft.easybiglau
                     }
                 }
             }
-        }
     } else {
         // List Screen
         Scaffold(
